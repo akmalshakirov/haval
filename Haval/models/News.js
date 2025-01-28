d87b565ee302f08706abef55e062cd066af862a2
Kumush-shining-nikki <kumushizziyeva@gmail.com>
2025-01-28 17:26:20 +0500

    ....................;)

--- Haval/models/News.js
+++ Haval/models/News.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose")
 
 const News = new mongoose.Schema({
-    newsId: {type: mongoose.Schema.Types.ObjectId},
     title: {type: String, required: true},
     description: {type: String, required: true},
     image: {type: String, required: true},
     createdAt: {type: String, required: true},
     updatedAt: {type: String},
-    role: { type: String, default: "admin" }
 })
 
 module.exports = mongoose.model('News', News);
