3494aac0896cae3e4964bb58f79df5c414af2982
akmal <akmalpubg221@gmail.com>
2025-03-26 20:01:20 +0500

    .

--- src/Pages/User/User.jsx
+++ src/Pages/User/User.jsx
@@ -272,6 +272,7 @@ function UserPage() {
                         </div>
                     </Header>
 
+                    {/* loading */}
                     {loading ? (
                         <div
                             style={{
