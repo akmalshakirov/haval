c61f9ff1e7cb4d7beb06569e36c675840e45c6de
akmal <144832892+akmalshakirov@users.noreply.github.com>
2025-06-12 16:38:11 +0500

    Update loginLimiter.js

--- Haval/middlewares/loginLimiter.js
+++ Haval/middlewares/loginLimiter.js
@@ -1,7 +1,7 @@
 const rateLimit = require("express-rate-limit");
 
 exports.loginLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, 
+    windowMs: 1 * 60 * 1000, 
     max: 10, 
 
     handler: (req, res ,next) => {
